92417f118bb0dfcab754c489c3dba1d5c16ffdc1
tar <private@mothzarella.dev>
2025-05-30 00:57:33 +0200

    test nix flake v2

--- flake.nix
+++ flake.nix
@@ -1,58 +1,42 @@
 {
-  description = "Basic Neovim configuration";
-
-  # To easily generate a derivation per architecture
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
+  description = "Neovim with flakes";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs = { self, nixpkgs, flake-utils }:
-    {
-      # We define first an overlay, i.e. a definition of new packages as recommended in
-      # https://discourse.nixos.org/t/how-to-consume-a-eachdefaultsystem-flake-overlay/19420/9
-      overlays.default = final: prev: {
-        yourprogram = final.callPackage ({ stdenv, pkgs, ... }:
-          # You can put here the derivation to build your program, for instance:
-          stdenv.mkDerivation {
-            src = ./.;
-            pname = "tar_nvim";
-            version = "unstable";
-            buildInputs = with pkgs; [
-              neovim
-
-              ripgrep
-              fd
-              fzf
-              tree
-            ];
-          }) { };
-      };
-    }
-    // # We now add the package defined in the above overlay for all architectures
-    (flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          system = system;
-          overlays = [ self.overlays.default ];
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
+        neovim-configured = pkgs.neovim.override { viAlias = true; };
       in {
-        # Create a new package
         packages = {
-          tar_nvim = pkgs.nvim;
-          default =
-            self.packages.${system}.tar_nvim; # default program: this way, typing "nix develop" will directly put you in a shell needed to develop the above your program, running "nix build/run" will directly build/run this program etc.
+          default = neovim-configured;
+          neovim = neovim-configured;
         };
 
-        homeManagerModules.default = { config, lib, pkgs, ... }: {
-          home = {
-            file.".config/nvim" = {
-              source = ./lua;
-              recursive = true;
-            };
-
-            shellAliases = {
-              vi = "nvim";
-              vim = "nvim";
-            };
+        apps = {
+          default = {
+            type = "app";
+            program = "${pkgs.writeShellScript "nvim-with-local-config" ''
+              exec ${neovim-configured}/bin/nvim --cmd "set runtimepath^=${
+                ./.
+              }/config" "$@"
+            ''}";
           };
         };
-      }));
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            neovim-configured
+            ripgrep
+            fd
+            fzf
+            git
+            gcc
+            nodejs
+            python3
+          ];
+        };
+      });
 }
